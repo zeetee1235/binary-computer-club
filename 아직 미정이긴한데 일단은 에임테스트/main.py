8eaee4a2de7e016d4444e4632bc2523b5f15b551
이건형 <102940970+zeetee1235@users.noreply.github.com>
2022-12-23 00:59:23 +0900

    Add files via upload

---
+++ 아직 미정이긴한데 일단은 에임테스트/main.py
@@ -0,0 +1,18 @@
+import pygame
+pygame.init()
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+LIGHT_GRAY = (200, 200, 200)
+DARK_GRAY = (100, 100, 100)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+FPS = 60
+
+font = pygame.font.SysFont("malgungothic",80)
+
+size = [1000,1000]
+screen = pygame.display.set_mode(size)
+
+
+
